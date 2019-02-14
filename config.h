767dfaf6fcc0b185127498793f85cc6a3f4cf66e
Byron J Williams <byron.w@ieee.org>
2019-02-13 19:02:06 -0500

    added selfrestart and modified size settings

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 22;        /* pixel gap between clients */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Inconsolata Nerd Font:size=28" };
+static const char *fonts[]          = { "Inconsolata Nerd Font:size=12" };
 static const char dmenufont[]       = "Inconsolata Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -44,10 +44,12 @@ static const Rule rules[] = {
 	{ "google-chrome",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Mailspring",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "GitKraken",  NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "google-keep",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Google Keep",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Chromium",  NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "Evernote Web",  NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "evernote",  NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "messenger",  NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Caprine",  NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "crx_hmjkmjkepdijhoojdojkdfohbdgmmhki",  NULL,       NULL,       1 << 7,       0,           -1 },
 	{ "Slack",  NULL,       NULL,       1 << 7,       0,           -1 },
 };
 
@@ -79,9 +81,12 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_recency", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+/* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 static const char *termcmd[]  = { "st", NULL };
 static const char *blurlock[]  = { "blurlock", NULL };
+
+#include "selfrestart.c"
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -116,6 +121,14 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+  { MODKEY,					XK_Down,	moveresize,		{.v = (int []){ 0, 25, 0, 0 }}},
+  { MODKEY,					XK_Up,		moveresize,		{.v = (int []){ 0, -25, 0, 0 }}},
+  { MODKEY,					XK_Right,	moveresize,		{.v = (int []){ 25, 0, 0, 0 }}},
+  { MODKEY,					XK_Left,	moveresize,		{.v = (int []){ -25, 0, 0, 0 }}},
+  { MODKEY|ShiftMask,			XK_O,	moveresize,		{.v = (int []){ 0, 0, 0, 25 }}},
+  { MODKEY|ShiftMask,			XK_S,		moveresize,		{.v = (int []){ 0, 0, 0, -25 }}},
+  { MODKEY|ShiftMask,			XK_E,	moveresize,		{.v = (int []){ 0, 0, 25, 0 }}},
+  { MODKEY|ShiftMask,			XK_Y,	moveresize,		{.v = (int []){ 0, 0, -25, 0 }}},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -125,6 +138,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+    { MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
 	{ MODKEY|ShiftMask,             XK_Escape,      quit,           {0} },
 };
 
