5a173e75910c0b9543d8334347cd84d1f64a0b16
Byron J. Williams <me@drbyron.io>
2019-02-13 19:29:31 -0500

    remove binary files and open google keep in tag 3

--- config.h
+++ config.h
@@ -41,7 +41,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "google-chrome",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Google-chrome",  NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "Mailspring",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "GitKraken",  NULL,       NULL,       1 << 6,       0,           -1 },
 	{ "Google Keep",  NULL,       NULL,       1 << 2,       0,           -1 },
