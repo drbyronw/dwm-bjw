aaf08a3a2495cde74ade88b20eeb31ba226cb710
Byron J. Williams <me@drbyron.io>
2019-02-15 05:11:38 -0500

    added tcl layout and modified cycle layout keys

--- config.h
+++ config.h
@@ -56,6 +56,7 @@ static const Rule rules[] = {
 
 /* layout(s) */
 #include "layouts.c"
+#include "tcl.c"
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
@@ -66,6 +67,7 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
   { "HHH",      grid },
+  { "|||",      tcl },
   { NULL,       NULL },
 };
 
@@ -118,8 +120,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY|ControlMask,     XK_comma,        cyclelayout,    {.i = -1 } },
-	{ MODKEY|ControlMask,     XK_period,       cyclelayout,    {.i = +1 } },
+	{ MODKEY,                       XK_comma,        cyclelayout,    {.i = -1 } },
+	{ MODKEY,                       XK_period,       cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
