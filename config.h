8611dc8851a016de7febdca0f5d178c543e696bb
$GITHUB_NAME <$GITHUB_EMAIL>
2019-02-22 15:42:35 -0500

    changed locking tool and added clipmenu shortcut

--- config.h
+++ config.h
@@ -41,8 +41,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Google-chrome",  NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "google-calendar-nativefier-9e2d0d",  NULL,       NULL,       1 << 2,       0,           -1 },
+	/* { "Google-chrome",  NULL,       NULL,       1 << 2,       0,           -1 }, */
+	{ "google-calendar-nativefier-9e2d0d",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Mailspring",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "GitKraken",  NULL,       NULL,       1 << 6,       0,           -1 },
 	{ "Google Keep",  NULL,       NULL,       1 << 2,       0,           -1 },
@@ -87,8 +87,9 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 /* static const char *dmenucmd[] = { "dmenu_recency", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-/* static const char *blurlock[]  = { "blurlock", NULL }; */
-static const char *lightdm_lock[]  = { "dm-tool", "lock", NULL };
+static const char *blurlock[]  = { "blurlock", NULL };
+static const char *clipmenu[]  = { "clipmenu", NULL };
+/* static const char *lightdm_lock[]  = { "dm-tool", "lock", NULL }; */
 
 #include "selfrestart.c"
 
@@ -98,8 +99,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	/* { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } }, */
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lightdm_lock } },
-	/* { MODKEY|ShiftMask,             XK_l,      spawn,          {.v = blurlock } }, */
+	/* { MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lightdm_lock } }, */
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = blurlock } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -110,7 +111,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_c,      spawn,          {.v = clipmenu } },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
