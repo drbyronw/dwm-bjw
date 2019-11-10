d3fa9c175f278efe437ef50274dffd34b2962528
Byron J. Williams <me@drbyron.io>
2019-11-10 13:57:57 -0500

    updated terminal default

--- config.h
+++ config.h
@@ -40,7 +40,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
 	/* { "Google-chrome",  NULL,       NULL,       1 << 2,       0,           -1 }, */
 	{ "google-calendar-nativefier-9e2d0d",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Mailspring",  NULL,       NULL,       1 << 8,       0,           -1 },
@@ -87,7 +87,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /* static const char *dmenucmd[] = { "dmenu_recency", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "termite", NULL };
 static const char *blurlock[]  = { "blurlock", NULL };
 static const char *clipmenu[]  = { "clipmenu", NULL };
 /* static const char *lightdm_lock[]  = { "dm-tool", "lock", NULL }; */
