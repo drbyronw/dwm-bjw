b84387607ed6716a37a5244e48d63f0d246a3b7e
Byron J. Williams <me@drbyron.io>
2021-03-08 15:28:51 -0500

    dwm config changes and updated Makefile to delete config.h

--- config.def.h
+++ config.def.h
@@ -98,7 +98,7 @@ static const char *dmenucmd[]    = { "dmenu_recency", "-p", "Run: ", NULL };
 
 /* the st terminal with tabbed */
 /* static const char *termcmd[]     = { "st", NULL }; */
-static const char *termcmd[]     = { "xfce4-terminal", NULL };
+static const char *termcmd[]     = { "alacritty", NULL };
 /* An alternative way to launch st along with the fish shell */
 /* static const char *termcmd[]     = { "st", "-e fish", NULL }; */
 static const char *tabtermcmd[]  = { "tabbed", "-r", "2", "st", "-w", "''", NULL };
